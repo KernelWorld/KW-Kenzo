757f5182b271ce3b3c3fce26e34723ff81e2f86f
War_machine28 <ideologuejyotirmay@gmail.com>
2016-04-28 18:39:00 +0530

    Revert "sched: initialize frequency domain cpumask"
    This reverts commit 2ac0971840e05f475df5941eb5210e06b2f63fc8.

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -9970,11 +9970,6 @@ void __init sched_init(void)
 		rq->wakeup_energy = 0;
 #ifdef CONFIG_SCHED_HMP
 		cpumask_set_cpu(i, &rq->freq_domain_cpumask);
-		rq->cur_freq = 1;
-		rq->max_freq = 1;
-		rq->min_freq = 1;
-		rq->max_possible_freq = 1;
-		rq->max_possible_capacity = 0;
 		rq->hmp_stats.cumulative_runnable_avg = 0;
 		rq->window_start = 0;
 		rq->hmp_stats.nr_small_tasks = rq->hmp_stats.nr_big_tasks = 0;
